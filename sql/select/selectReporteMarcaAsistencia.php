7d6868ec9b93b4453e8728fa6272096d0bb8266a
mbermudezv <-C>
2023-06-21 14:21:25 -0600

    asistencia cocineros

---
+++ sql/select/selectReporteMarcaAsistencia.php
@@ -0,0 +1,63 @@
+<?php
+
+class SelectReporteMarcaAsistencia
+{
+
+    private $pdo;
+       	
+	function __construct()
+	{
+
+        $pdo = new \PDO(DB_Str, DB_USER, DB_PASS, 
+                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                    
+        $this->pdo = $pdo;        
+        
+    }
+
+    function selectReporteMarcaAsistencia($fechaDesde, $fechaHasta)
+    {        
+
+        if ($this->pdo != null) {
+            
+            date_default_timezone_set('America/Costa_Rica');
+	 	    $fechaDesdeFormato = date_create($fechaDesde)->format('Y-m-d');
+            $fechaHastaFormato = date_create($fechaHasta)->format('Y-m-d');
+
+            $consultaSQL = "SELECT persona.persona_nombre, 
+                            tipoMarcaAsistencia.tipoMarcaAsistencia_Descripcion, 
+                            Marca_Asistencia_Fecha, Marca_Asistencia_Hora 
+                            FROM Marca_Asistencia 
+                            INNER JOIN persona ON 
+                            Marca_Asistencia.persona_id = persona.persona_id
+                            INNER JOIN tipoMarcaAsistencia ON 
+                            Marca_Asistencia.tipoMarcaAsistencia_id = tipoMarcaAsistencia.tipoMarcaAsistencia_id 
+                            WHERE Marca_Asistencia_Fecha BETWEEN '".$fechaDesdeFormato."' AND '".$fechaHastaFormato."' 
+                            ORDER BY Marca_Asistencia.persona_id, 
+                            Marca_Asistencia_Fecha, Marca_Asistencia.tipoMarcaAsistencia_id";                                       
+
+			$sql = $this->pdo->query($consultaSQL);
+
+			$rs = [];
+
+			while ($row = $sql->fetch(\PDO::FETCH_ASSOC)) {
+					$rs[] = [						
+                        'Marca_Fecha' => $row['Marca_Fecha'],
+						'Estudiante_Nombre' => $row['Estudiante_Nombre'],
+                        'Estudiante_Apellido1' => $row['Estudiante_Apellido1'],
+                        'Estudiante_Apellido2' => $row['Estudiante_Apellido2'],
+                        'seccion_Descripcion' => $row['seccion_Descripcion']                        
+					];
+			}
+
+            return $rs;
+
+		}   
+
+		$this->pdo = null;
+
+    }
+    
+}
+
+?>
