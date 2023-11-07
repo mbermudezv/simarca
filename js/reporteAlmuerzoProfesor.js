3b25e6409c94aaec946bc0bc4f1050c12b29e5cd
mbermudezv <mauriciobermudez@hotmail.com>
2023-11-07 11:41:10 -0600

    formato fecha

--- js/reporteAlmuerzoProfesor.js
+++ js/reporteAlmuerzoProfesor.js
@@ -119,8 +119,8 @@ function cargaDatosPantalla(data)
         let monto = obj.Monto;
 
         let date = new Date(obj.Fecha);
-        let day = `${(date.getDate())}`.padStart(2,'0');
-        let month = `${(date.getMonth()+1)}`.padStart(2,'0');
+        let day = `${(date.getUTCDate())}`.padStart(2,'0');
+        let month = `${(date.getUTCMonth()+1)}`.padStart(2,'0');
         let year = date.getFullYear();
                
         let fechaFormato = `${day}-${month}-${year}`;
